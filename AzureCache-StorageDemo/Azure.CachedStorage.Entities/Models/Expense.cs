bc3e3a3442cf7dd8dd01c9ceea80c32ff0cc2cc5
Swinkaran <swinkaran@hotmail.com>
2019-01-10 16:52:08 +1100

    Generic IEntity created
    Entities added
    - IEntity
    - Expense entity

---
+++ AzureCache-StorageDemo/Azure.CachedStorage.Entities/Models/Expense.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Azure.CachedStorage.Entities.Models
+{
+    [Table("Expense")]
+    public class Expense : IEntity
+    {
+        [Key]
+        [Column("ExpenseId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(60, ErrorMessage = "Name can't be longer than 60 characters")]
+        public string Name { get; set; }
+
+        [StringLength(100, ErrorMessage = "Description can't be longer than 100 characters")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Amount is required")]
+        public float Amount { get; set; }
+
+        [Required(ErrorMessage = "Date is required")]
+        public float Date { get; set; }
+    }
+}
